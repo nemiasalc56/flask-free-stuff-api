25213c9781f2d7e7c12248cea86311602e666667
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-02-23 22:24:18 -0600

    we can run the server

---
+++ app.py
@@ -0,0 +1,25 @@
+# import flask
+from flask import Flask
+
+
+
+
+DEBUG = True # print the error
+PORT = 8000
+
+
+
+
+
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=DEBUG, port=PORT) 
