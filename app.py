be8232c1e1b6236232c34c2985ca441623ffc2f3
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-02-24 10:18:17 -0600

    app connects to database and creates tables

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # import flask
 from flask import Flask
+import models
 
 
 
@@ -21,4 +22,5 @@ app = Flask(__name__)
 
 
 if __name__ == '__main__':
+	models.initialize()
 	app.run(debug=DEBUG, port=PORT) 
--- models.py
+++ models.py
@@ -4,7 +4,8 @@ import datetime
 
 
 
-
+# using sqlite to have a database
+DATABASE = SqliteDatabase('items.sqlite')
 
 
 
@@ -26,6 +27,18 @@ class Item(Model):
 	name = CharField()
 	picture = CharField()
 	address = CharField()
-	owner = ForeignKeyField(User, backref='items')
+	owner = CharField() # we will add this ForeignKeyField(User, backref='items') late here
 	created_at = DateTimeField(default=datetime.datetime.now)
 
+	# this gives our class instructions on how to connect to a specific databse
+	class Meta:
+		database = DATABASE
+
+# this method will set up the connection to our database
+def initialize():
+	DATABASE.connect()
+	DATABASE.create_tables([Item], safe=True)
+
+	print("Connected to database and created tables if they weren't already there.")
+
+	DATABASE.close()
