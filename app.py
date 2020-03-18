639bb72c835b71a8e37f077c0b71b70b3e800d4a
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-03-17 19:39:48 -0500

    debuggin app

--- app.py
+++ app.py
@@ -54,20 +54,6 @@ def unauthorized():
 		), 403
 
 
-# use this decorator to cause a function to run before request
-@app.before_request
-def before_request():
-	# store the data as a global variable in g
-	g.db = models.DATABASE
-	g.db.connect()
-
-
-# use this decorator to cause a function to run after request
-@app.after_request
-def after_request(response):
-	g.db.close()
-	return response
-
 CORS(users, origins=['http://localhost:3000', 'https://thefreestuff.herokuapp.com'], supports_credentials=True)
 CORS(items, origins=['http://localhost:3000', 'https://thefreestuff.herokuapp.com'], supports_credentials=True)
 CORS(comments, origins=['http://localhost:3000', 'https://thefreestuff.herokuapp.com'], supports_credentials=True)
@@ -78,6 +64,19 @@ app.register_blueprint(items, url_prefix='/api/v1/items/')
 app.register_blueprint(comments, url_prefix='/api/v1/comments/')
 
 
+# # use this decorator to cause a function to run before request
+# @app.before_request
+# def before_request():
+# 	# store the data as a global variable in g
+# 	g.db = models.DATABASE
+# 	g.db.connect()
+
+
+# # use this decorator to cause a function to run after request
+# @app.after_request
+# def after_request(response):
+# 	g.db.close()
+# 	return response
 
 
 
